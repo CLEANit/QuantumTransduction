60e56a3bd52d4eb95a7c981e05550467677ee349
Kevin Ryczko <kryczko@nia-login07.scinet.local>
2019-05-24 14:52:07 -0400

    adding up summarizing script for many calculations, and bug fix in input file generator

--- analysis/summarizeCalcs.py
+++ analysis/summarizeCalcs.py
@@ -4,8 +4,9 @@ import numpy as np
 import cmocean.cm as cm
 import sys
 import subprocess
-
+import matplotlib.pyplot as plt
 import progressbar as pb
+import matplotlib.colors as colors
 
 bar = pb.ProgressBar()
 
@@ -13,8 +14,27 @@ files = subprocess.check_output('find . -name phase*', shell=True)
 
 dir_to_file = {}
 
-for elem in bar(files.split()):
-	elem = elem.decode('utf-8')
-	dir_to_file[elem.split('/')[1]] = elem.split()[-1]
+for elem in files.split():
+    elem = elem.decode('utf-8')
+    dir_to_file[elem.split('/')[1]] = elem
 
-print(dir_to_file)
+
+dir_to_data = {}
+for key, val in dir_to_file.items():
+    dir_to_data[key] = np.loadtxt(val)
+
+data_x = []
+data_y = []
+cs = []
+
+for key, val in dir_to_data.items():
+    objs = (val[:,0]**2 + val[:,1]**2 + val[:,2]**2)**0.5
+    obj = np.max(objs)
+    data_x.append(float(key.split('_')[2]))
+    data_y.append(float(key.split('_')[6]))
+    cs.append(obj)
+
+plt.scatter(data_x, data_y, c=cs, cmap=cm.deep, s=500)
+plt.colorbar()
+plt.show()
+ 
