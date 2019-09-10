072a04c4407f08a0e6ec8174d513e4648b9d9180
Kevin Ryczko <kryczko@stormtrooper.local>
2019-09-10 16:59:46 -0400

    fixed tab problem

--- analysis/siteDistro.py
+++ analysis/siteDistro.py
@@ -16,14 +16,14 @@ file_list = subprocess.check_output('find . -name "ga.dill"', shell=True).split(
 site_distro = {}
 bar = progressbar.ProgressBar()
 for filename in bar(file_list):
-	ga = dill.load(open(filename.decode('utf-8'), 'rb'))
-	structures = ga.rankGenerationWithSquare()
+    ga = dill.load(open(filename.decode('utf-8'), 'rb'))
+    structures = ga.rankGenerationWithSquare()
 
-	for s in structures:
-		colours = s.sytem_colours
-		for site, value in system.site_value_pairs():
+    for s in structures:
+        colours = s.sytem_colours
+        for site, value in system.site_value_pairs():
             if s.body(site.pos):
-            	if site not in site_distro:
-            		site_distro[site] = []
-            	site_distro[site].append(colours[site])
+                if site not in site_distro:
+                    site_distro[site] = []
+                site_distro[site].append(colours[site])
 print(site_distro)
